872fac72d84405664b0b0969ec2c8001ef4f9b6e
faruqalawy <faruqhudaya@gmail.com>
2023-12-07 00:45:11 +0700

    third commit

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -4,11 +4,11 @@ import propTypes from 'prop-types'
 
 export default function Button(props) {
     const className = [props.className]
-    if(props.isPrimary) className.push("btn-primary")
-    if(props.isLarge) className.push("btn-lg")
-    if(props.isSmall) className.push("btn-sm")
-    if(props.isBlock) className.push("btn-block")
-    if(props.hasShadow) className.push("btn-shadow")
+    if(props.isPrimary) className.push(" btn-primary")
+    if(props.isLarge) className.push(" btn-lg")
+    if(props.isSmall) className.push(" btn-sm")
+    if(props.isBlock) className.push(" btn-block")
+    if(props.hasShadow) className.push(" btn-shadow")
 
     const onClick = () => {
         if(props.onClick) props.onClick()
@@ -77,6 +77,7 @@ Button.propTypes ={
     href: propTypes.string,
     target: propTypes.string,
     className: propTypes.string,
+    isPrimary: propTypes.bool,
     isDisable: propTypes.bool,
     isLoading: propTypes.bool,
     isSmall: propTypes.bool,
