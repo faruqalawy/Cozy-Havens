c98c677e7e37ab60d908a0e9d94eccfcd41840d5
faruqalawy <faruqhudaya@gmail.com>
2023-12-07 01:04:37 +0700

    fixing index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
+
+root.unstable_createRoot().render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
