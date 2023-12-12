b11d05987eaa921cdf7a4c3484d12962c8dbe08b
faruqalawy <faruqhudaya@gmail.com>
2023-12-12 14:20:14 +0700

    create footer component

--- src/elements/Star/index.js
+++ src/elements/Star/index.js
@@ -1,58 +1,49 @@
-import React from 'react'
-
-import propTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./index.scss";
 
 export default function Star({ className, value, height, width, spacing }) {
-    const decimals = Number(value) % 1;
+    const wholeStars = Math.floor(value);
+    const decimals = value - wholeStars;
 
-    const star = [];
-    let leftPos = 0;
-    for (let index = 0; index < 5 && index < value - decimals; index++) {
-        leftPos = leftPos + width;
-        star.push(
-            <div 
-                className='star'
-                key={`star-${index}`}
-                style={{left: index * width, width: width, marginRight: spacing}}
-            ></div>
-        );
-    }
-    if (decimals > 0 && value <= 5)
-        star.push(
-            <div 
-                className='star'
-                key={`star-${index}`}
-                style={{left: leftPos * width, width: width - spacing}}
-            ></div>
-        );
+    const stars = Array.from({ length: 5 }, (_, index) => (
+        <div
+            className={`star ${index < wholeStars ? '' : 'placeholder'}`}
+            key={`star-${index}`}
+            style={{
+                left: index * (width + spacing),
+                height: height,
+                width: width,
+                marginRight: index < 4 ? spacing : 0
+            }}
+        ></div>
+    ));
 
-    const starPlaceholder = [];
-    for (let index = 0; index < 5; index++) {
-        starPlaceholder.push(
-            <div 
-                className='star placeholder'
-                key={`starPlaceholder-${index}`}
-                style={{left: index * width, width: width, marginRight: spacing}}
-            ></div>
-        );
-    }
-    
-    return (
-        <>
+    if (decimals > 0 && value <= 5) {
+        stars.push(
             <div
-                className={['stars', className].join(" ")}
-            >
-                {starPlaceholder}
-                {star}
-            </div>
-        </>
-    ) 
+                className='star'
+                key={`starWithDecimal`}
+                style={{
+                    left: wholeStars * (width + spacing),
+                    height: height,
+                    width: decimals * width - spacing,
+                }}
+            ></div>
+        );
+    }
+
+    return (
+        <div className={['stars', className].join(" ")}>
+            {stars}
+        </div>
+    );
 }
 
 Star.propTypes = {
-    className: propTypes.string, 
-    value: propTypes.number,
-    width: propTypes.number,
-    height: propTypes.number,
-    spacing: propTypes.number
-}
+    className: PropTypes.string,
+    value: PropTypes.number,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    spacing: PropTypes.number
+};
---
+++ src/parts/Testimony.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import TestimonyAccent from 'assets/images/testimonial-image-frame.jpg'
+
+import Star from 'elements/Star'
+import Button from 'elements/Button'
+
+export default function Testimony({data}) {
+    return (
+        <section className='container'>
+            <div className='row align-items-center'>
+                <div className='col-auto' style={{marginRight: 30}}>
+                    <div 
+                        className='testimonial-hero'
+                        style={{margin: `30px 0 0 30px`}}
+                    >
+                        <img src={data.imageUrl} alt="Testimonial" className='position-absolute' style={{zIndex: 1}} />
+                        <img src={TestimonyAccent} alt='Testimonial frame' className='position-absolute' style={{margin: `-30px 0 0 -30px`}} />
+                    </div>
+                </div>
+                <div className='col'>
+                    <h4 style={{marginBottom: 30}}>
+                        {data.name}
+                    </h4>
+                    <Star value={data.rate} width={48} height={48} spacing={4} />
+                    <h5 className='font-weight-light line-height-2 my-3' style={{fontSize: 28}}>
+                        {data.content}
+                    </h5>
+                    <span className='text-gray-500'>
+                        {data.travelerName}, {data.travelerOccupation}
+                    </span>
+                    <div>
+                        <Button className='btn px-5' style={{marginTop: 65}} hasShadow isPrimary type='link' href={`/testimonial/${data._id}`}>
+                            Read His Story
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
